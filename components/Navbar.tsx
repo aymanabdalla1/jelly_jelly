71e0ddaa99ad638b2f5794c23295686969c4151b
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-08-24 19:36:54 -0500

    working on page styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,26 +14,18 @@ export default function Navbar() {
           height={36}
         />
         <h1 className="sm:text-5xl text-3xl font-bold ml-2 tracking-tight text-white hover:text-white/50">
-          CaptionAI
+          CaptionFlow
         </h1>
       </Link>
-      <a
-        className="relative overflow-hidden rounded-full bg-black px-20 sm:px-24 py-6 ring-red-500/50 ring-offset-black will-change-transform focus:outline-none focus:ring-1 focus:ring-offset-2"
-        href="https://github.com/hqasmei/captionAI"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <span className="absolute inset-px z-10 grid place-items-center rounded-full bg-black bg-gradient-to-t from-neutral-800 text-neutral-400">
-          <div className="flex flex-row space-x-2 items-center">
-            <AiOutlineGithub color="white" size={25} />
-            <span className="text-gray-100">Star on Github</span>
-          </div>
-        </span>
-        <span
-          aria-hidden
-          className="absolute inset-0 z-0 scale-x-[2.0] blur before:absolute before:inset-0 before:top-1/2 before:aspect-square before:animate-disco before:bg-gradient-conic before:from-purple-700 before:via-red-500 before:to-amber-400"
-        />
-      </a>
+      <div className="flex space-x-2 items-center">
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Login
+        </button>
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          Signup
+        </button>
+      </div>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,14 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>CaptionAI</title>
+        <title>CaptionFlow</title>
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💭</text></svg>"
         />
       </Head>
 
-      <div className="bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <div className="bg-gradient-to-b from-[#191970] to-[#15162c]">
         <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
           <Navbar />
           <HeroSection />
