39bf5d3a3485cd5f4d31fc979c2eac0a0084f2c5
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-08-25 01:08:05 -0500

    styling this thing

--- src/components/PageHeaders.js
+++ src/components/PageHeaders.js
@@ -4,12 +4,15 @@ export default function PageHeaders({
 }) {
   return (
     <section className="text-center mt-12 sm:mt-24 mb-4 sm:mb-8">
-      <h1 className="text-xl sm:text-3xl" style={{textShadow: '1px 1px 0 rgba(0,0,0,.2)'}}>
-        {h1Text}
-      </h1>
-      <h2 className="text-white/75 text-sm sm:text-base">
-        {h2Text}
+      <h2 className="mx-auto max-w-3xl font-display text-3xl font-bold tracking-normal sm:text-7xl text-white">
+        Easily add captions to your videos
+        <span className="relative whitespace-nowrap text-[#7DF9FF]">
+          <span className="relative"> using AI.</span>
+        </span>
       </h2>
+      <p className="mx-auto mt-4 md:mt-12 max-w-xl text-lg text-stone-400 leading-7">
+        Take your videos to the next level with custom captions.
+      </p>
     </section>
   );
 }
